420a5370ad980f260630b4aa809e62a9c26597e7
Marcel Heyne <32398058+marcelheyne@users.noreply.github.com>
2022-05-24 14:03:22 +0200

    Add files via upload

---
+++ 101/audio.js
@@ -0,0 +1,216 @@
+Amplitude.init({
+   "songs":[
+      {
+         "name":"Qulqullinni akkamiti dhukkuba irraa nu eega?",
+         "url":"./mp3/010101.mp3"
+      },
+      {
+         "name":"Jarmiin dhukkuba fidu akkamiti tamsa’uu dandaa’a?",
+         "url":"./mp3/010102.mp3"
+      },
+      {
+         "name":"Qulqullini akkamiti tamsa’ina jarmii nurraa dhorga?",
+         "url":"./mp3/010103.mp3"
+      },
+      {
+         "name":"Qulqullinni naannoo akkamitti dhukkuba nurraa eega?",
+         "url":"./mp3/010104.mp3"
+      },
+      {
+         "name":"Bishaan dhugaati fi bishaan soorata ittin qophaa’u akkamiti qulquuna isaa eegana?",
+         "url":"./mp3/010105.mp3"
+      },
+      {
+         "name":"Balfa adda adda haala  kamiin gatuu qabana?",
+         "url":"./mp3/010106.mp3"
+      },
+      {
+         "name":"Balfa adda adda haala  kamiin gatuu qabana?",
+         "url":"./mp3/010107.mp3"
+      },
+      {
+         "name":"Maaliif bishaan bakka tokkoti chisee jiru deemsisuun barbaachise?",
+         "url":"./mp3/010108.mp3"
+      },
+      {
+         "name":"Akkamiiti mana fincaani ijaaru dandeenya?",
+         "url":"./mp3/010109.mp3"
+      },
+      {
+         "name":"Qulqullina maana keesa eeguun akkamiti dhukkuba irra nu eega?",
+         "url":"./mp3/010110.mp3"
+      },
+      {
+         "name":"Qaama ofii eegun akkamiti dhukkuba irraa nu eeguu danda’a?",
+         "url":"./mp3/010111.mp3"
+      },
+      {
+         "name":"Xurii laguu irra akkamiti of qulqulleesu dandeena?",
+         "url":"./mp3/010112.mp3"
+      },
+      {
+         "name":"Qulqullina ilkaani akami eegana?",
+         "url":"./mp3/010113.mp3"
+      },
+      {
+         "name":"Quluqullina ija keena akkamiti eegachu qabna?",
+         "url":"./mp3/010114.mp3"
+      },
+      {
+         "name":"Bishaan dhugaati qulqullu akkamiti argachuu dandeena?",
+         "url":"./mp3/010115.mp3"
+      },
+      {
+         "name":"Bishaan dhugaati akkamiti qulquleesina?",
+         "url":"./mp3/010116.mp3"
+      },
+      {
+         "name":"Nyaata keena akkamiti eegu dandeena?",
+         "url":"./mp3/010117.mp3"
+      },
+      {
+         "name":"Tatamsaa’ina jarmii nyaata irra akkamti eegu dandeena?",
+         "url":"./mp3/010118.mp3"
+      },
+      {
+         "name":"Midhaan badde akkamiti beeku dandeena?",
+         "url":"./mp3/010119.mp3"
+      },
+      {
+         "name":"Nyaata akkamiti seeran bilcheesu dandeena?",
+         "url":"./mp3/010120.mp3"
+      },
+      {
+         "name":"Akkamiti nyaata gaari filaachuu dandeena?",
+         "url":"./mp3/010121.mp3"
+      },
+      {
+         "name":"Nyaata akkamiti seeraan kaa’uu dandeena?",
+         "url":"./mp3/010122.mp3"
+      },
+      {
+         "name":"Nyaata akkamitu tursiisuu dandeena?",
+         "url":"./mp3/010123.mp3"
+      },
+      {
+         "name":"Nyaata akkamiti daa’imaaniif qopheesu qabna?",
+         "url":"./mp3/010124.mp3"
+      },
+      {
+         "name":"Nyaani madaalawwan fayyaa keenaaf maaf barbbachisa?",
+         "url":"./mp3/010401.mp3"
+      },
+      {
+         "name":"Nyaata madaalawaa jechuun maal jechuudha?",
+         "url":"./mp3/010402.mp3"
+      },
+      {
+         "name":"Guyya guyyan soorata akkami nyaachu (argachu) qaba?",
+         "url":"./mp3/010403.mp3"
+      },
+      {
+         "name":"Vitaaminootafi miniraalota keessaa kamtuu baa’inaan nu barbaachisa?",
+         "url":"./mp3/010404.mp3"
+      },
+      {
+         "name":"Ayiraanii gaha ta’e qaamni keenaa akkamiti argata?",
+         "url":"./mp3/010405.mp3"
+      },
+      {
+         "name":"Foolik asiidi gaha akkamiti argana?",
+         "url":"./mp3/010406.mp3"
+      },
+      {
+         "name":"Kaalsiyeemi gahaa akkamiti argana?",
+         "url":"./mp3/010407.mp3"
+      },
+      {
+         "name":"Akkamiti ayoodinii gaha argaachu dandeena?",
+         "url":"./mp3/010408.mp3"
+      },
+      {
+         "name":"Akkamiti ayoodini bulbuulamaa qopheesu dandeena?",
+         "url":"./mp3/010409.mp3"
+      },
+      {
+         "name":"Akkammitti vaaytaaminii A gaha arrgachuu dandeena?",
+         "url":"./mp3/010410.mp3"
+      },
+      {
+         "name":"Nyaata madaalawa qarshii salphaan akkamiti argachuu dandeena?",
+         "url":"./mp3/010411.mp3"
+      },
+      {
+         "name":"Yaada waayee soorta jiru keessa kan miidha qabu kam?",
+         "url":"./mp3/010412.mp3"
+      },
+      {
+         "name":"Nyaani madaalawwaa hin taane akkamiti dhukkubaaf nu saaxila?",
+         "url":"./mp3/010413.mp3"
+      },
+      {
+         "name":"Hanqqina dhiiga jechuun maal jechuudha?",
+         "url":"./mp3/010414.mp3"
+      },
+      {
+         "name":"Sababbni hanqqina dhiigaa fidan maal fa’a?",
+         "url":"./mp3/010415.mp3"
+      },
+      {
+         "name":"Dhukkuba hanqqinna dhiigaa akkamiti ittisna?",
+         "url":"./mp3/010416.mp3"
+      },
+      {
+         "name":"Beribeeriin maali?",
+         "url":"./mp3/010417.mp3"
+      },
+      {
+         "name":"Beribeerii akkamiti ittisuu dandeena?",
+         "url":"./mp3/010418.mp3"
+      },
+      {
+         "name":"Rakkowwan kan biroo nyaata haala malee nyaachuufi, nyaata hin fayyadne nyaachuun fidu maal fa’a?",
+         "url":"./mp3/010419.mp3"
+      },
+      {
+         "name":"Dhuukkubni sukaaraa maali?",
+         "url":"./mp3/010420.mp3"
+      },
+      {
+         "name":"Akkamitti dhibee sukkaaraa too’achuu dandenaa?",
+         "url":"./mp3/010421.mp3"
+      },
+      {
+         "name":"Haala akkamiin soorata madaalawwaa ittiin arganu fooyyesu dandeena?",
+         "url":"./mp3/010422.mp3"
+      },
+      {
+         "name":"Akkamitti ijolle tenaa rakkina guddinaaf dhibee sababa hanqinnaa nyaataatiin dhufu ittisa?",
+         "url":"./mp3/010423.mp3"
+      },
+      {
+         "name":"Daa’imaan ummriin isaani 6 ol ta’an akkamiti nyaachifana?",
+         "url":"./mp3/010424.mp3"
+      },
+      {
+         "name":"Nyaata akkamitu osoo hin baddin tursiisuu dandeena?",
+         "url":"./mp3/010425.mp3"
+      },
+      {
+         "name":"Fayidaan vaytamiin A daimaa’n kenaa qabuu mali?",
+         "url":"./mp3/010426.mp3"
+      },
+      {
+         "name":"Fayidaan ayreenii daima’aanif maal dhaa?",
+         "url":"./mp3/010427.mp3"
+      },
+      {
+         "name":"Fayidaan ayoodini  daimaa’nif qabuu malii?",
+         "url":"./mp3/010428.mp3"
+      },
+      {
+         "name":"Koronaan maalii? Of eeguufis maal gochuun qaba?",
+         "url":"./mp3/000001.mp3"
+      }
+   ]
+});
