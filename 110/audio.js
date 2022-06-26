3e46afa5322b9db2af0e8fc2e2f5f9df13406b43
Marcel Heyne <32398058+marcelheyne@users.noreply.github.com>
2022-06-26 19:57:52 +0200

    Added WHO Doing What Matters in Times of Stress
    Includes a new version of WOM.fm with support for showing the source of the corresponding audio.

---
+++ 110/audio.js
@@ -0,0 +1,53 @@
+Amplitude.init({
+   "songs":[
+      {
+         "name":"Stress Management - Building Awareness",
+         "url":"./audio/Building_Awareness.mp3",
+         "source": "World Health Organization",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      {
+         "name":"Stress Management - Grounding Exercise 1",
+         "url":"./audio/Grounding_Exercise_1.mp3",
+         "source": "World Health Organization",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      {
+         "name":"Stress Management - Grounding Exercise 2",
+         "url":"./audio/Grounding_Exercise_2.mp3",
+         "source": "World Health Organization",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      {
+         "name":"Stress Management - Grounding Exercise 3",
+         "url":"./audio/Grounding_Exercise_3.mp3",
+         "source": "World Health Organization",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      {
+         "name":"Stress Management - Notice and Name",
+         "url":"./audio/Notice_and_Name.mp3",
+         "source": "World Health Organization",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      {
+         "name":"Stress Management - Unhooking from Unkind Thoughts",
+         "url":"./audio/Unhooking_from_Unkind_Thoughts.mp3",
+         "source": "World Health Organization",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      {
+         "name":"Stress Management - Making Room",
+         "url":"./audio/Making_Room.mp3",
+         "source": "World Health Organization",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      {
+         "name":"Stress Management - Being Kind to Yourself",
+         "url":"./audio/Being_kind_to_yourself.mp3",
+         "source": "World Health Organization",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+  
+   ]
+});
