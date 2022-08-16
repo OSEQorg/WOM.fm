ca8f5ca96ad2e54b9f3c75d90ce7aca8e16a5a0b
Nyayic <chrosbiefannie@gmail.com>
2022-08-16 18:26:49 +0300

    901-TB in Lugbarati

---
+++ 901/README.md
@@ -0,0 +1,13 @@
+# WOM.fm-TB In Lugbarati.
+This is a collection of audio notes that are aimed at educating people about Tuberculosis(TB). 
+The audio notes are recorded in Lugbarati Language. 
+These audios explain;
+* What TB is.
+* The signs and symptoms of TB.
+* How TB is treated. 
+* Where one can seek help incase they are infected with this disease.
+
+
+You can check out the final app by clicking [here](https://wofm-tb-lugbarati.netlify.app/)
+
+
---
+++ 901/audio.js
@@ -0,0 +1,30 @@
+Amplitude.init({
+   "songs":[
+      {
+         "name":"What is TB?",
+         "url":"./audio/What is TB.mp3",
+         "source": "DAHW",
+         "source_url": "https://www.dahw.de/en/organization/vision-and-mission/about-us.html"
+      },
+      {
+         "name":"Signs and Symptoms of TB",
+         "url":"./audio/Signs and symptoms of TB.mp3",
+         "source": "DAHW",
+         "source_url": "https://www.dahw.de/en/organization/vision-and-mission/about-us.html"
+      },
+      
+      {
+         "name":"How is TB treated?",
+         "url":"./audio/How is TB treated.mp3",
+         "source": "DAHW",
+         "source_url": "https://www.dahw.de/en/organization/vision-and-mission/about-us.html"
+      },
+      {
+         "name":"Where to seek for help when infected",
+         "url":"./audio/Where to seek for help when infected.mp3",
+         "source": "DAHW",
+         "source_url": "https://www.dahw.de/en/organization/vision-and-mission/about-us.html"
+      }
+  
+   ]
+});
