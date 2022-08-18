4db800e2ba40bb40cf7edf76aee06e760fae20d1
Marcel Heyne <32398058+marcelheyne@users.noreply.github.com>
2022-08-18 12:28:00 +0200

    Update audio.js

--- 905/audio.js
+++ 905/audio.js
@@ -3,177 +3,177 @@ Amplitude.init({
       {
          "name":"Eby'endya",
          "url":"./audio/001.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
 
       {
          "name":"Omugaso gwa okulya bulungi",
          "url":"./audio/002.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Endya ennungi kitegeza ki ?",
          "url":"./audio/003.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Emmere jentekeddwa okulya bulijo",
          "url":"./audio/004.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Bikulu ki mu kuziyiza endwadde byenetagga ?",
          "url":"./audio/005.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Nkola ntya okulaba nti nfuna omusayi ogumala ?",
          "url":"./audio/006.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Nkola ntya okulaba nti nina folic acid amala ?",
          "url":"./audio/007.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Nkola ntya okulaba nti nina calcium amala ?",
          "url":"./audio/008.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Nkola ntya okulaba nti nina iodine amala ?",
          "url":"./audio/009.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Nkobola ntya okukola ekyokunywa kya iodine ?",
          "url":"./audio/010.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Nkakasa ntya nti nina ekirungo kya Vitamin A ?",
          "url":"./audio/011.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Nsobola ntya okulya obulungi naye nga nsasanya kitono ?",
          "url":"./audio/012.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Biki ebikwatagana n'ebyokulya eby'obulabe ?",
          "url":"./audio/013.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Endya embi eyinza okuleta ntya endwadde ?",
          "url":"./audio/014.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Okuggwa'mu omusayi kye'ki ?",
          "url":"./audio/015.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Biki ebireta anaemia ?",
          "url":"./audio/016.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Nyinza'ntya okuziyiza okugwamu omusayi ?",
          "url":"./audio/017.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Beri beri kye'ki ? ",
          "url":"./audio/018.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Nyinza'ntya okuziyiza beri beri ?",
          "url":"./audio/019.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Buzibu ki obulala obuva ku kulya emmere enyingi oba okulya ebika by'emmere ebi kyamu ?",
          "url":"./audio/020.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Obulwadde bwa sukali ky'eki ?",
          "url":"./audio/021.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Nsobola'ntya okufuga sukali ? ",
          "url":"./audio/022.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Kiki kyenina okukola okulongosa endya yange ?",
          "url":"./audio/023.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Oziyiza otya obuzibu mu kukula kwa'abaana, nga kuva ku endya embi ? ",
          "url":"./audio/024.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Ndisa'ntya abana abato, abasusiza emyezi 6 ?",
          "url":"./audio/025.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Emeere ki ennungi ku baana abato, abasuka emyezi 6 ?",
          "url":"./audio/026.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Lwaki ekirisa kya Vitamin A kyamugaso ku mwana wange ?",
          "url":"./audio/027.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Lwaki iron wa mugaso ku mwana wange ?",
          "url":"./audio/028.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       {
          "name":"Lwaki iodine wa mugaso ku mwana wange ?",
          "url":"./audio/029.mp3",
-         "source": "TPO Uganda",
-         "source_url": "https://tpoug.org/"
+         "source": "Audiopedia",
+         "source_url": "https://www.audiopedia.org/"
       },
       
    ]
