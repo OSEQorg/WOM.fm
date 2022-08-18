ed688e491210701fbb8a264731353a8f204550cc
Marcel Heyne <32398058+marcelheyne@users.noreply.github.com>
2022-08-18 12:24:45 +0200

    Add files via upload

---
+++ 905/README.md
@@ -0,0 +1,6 @@
+# WOM.fm-Nutrition in Luganda 
+
+WOM.fm-Nutriton in Luganda is an open platform for accessible audio information about Nutrition in Luganda language . [This website](https://endyaenungi.netlify.app/) has been created to make audio information about nutrition accessible to Luganda speaking populations using short urls and QR codes.
+This information is composed of 29 short audios about various nutrients, their purpose in the body and diseases caused by deficiency or excessive nutrients in one's diet.
+
+You can check out the final app by clicking [here](https://endyaenungi.netlify.app/). 
---
+++ 905/audio.js
@@ -0,0 +1,180 @@
+Amplitude.init({
+   "songs":[
+      {
+         "name":"Eby'endya",
+         "url":"./audio/001.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+
+      {
+         "name":"Omugaso gwa okulya bulungi",
+         "url":"./audio/002.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Endya ennungi kitegeza ki ?",
+         "url":"./audio/003.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Emmere jentekeddwa okulya bulijo",
+         "url":"./audio/004.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Bikulu ki mu kuziyiza endwadde byenetagga ?",
+         "url":"./audio/005.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Nkola ntya okulaba nti nfuna omusayi ogumala ?",
+         "url":"./audio/006.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Nkola ntya okulaba nti nina folic acid amala ?",
+         "url":"./audio/007.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Nkola ntya okulaba nti nina calcium amala ?",
+         "url":"./audio/008.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Nkola ntya okulaba nti nina iodine amala ?",
+         "url":"./audio/009.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Nkobola ntya okukola ekyokunywa kya iodine ?",
+         "url":"./audio/010.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Nkakasa ntya nti nina ekirungo kya Vitamin A ?",
+         "url":"./audio/011.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Nsobola ntya okulya obulungi naye nga nsasanya kitono ?",
+         "url":"./audio/012.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Biki ebikwatagana n'ebyokulya eby'obulabe ?",
+         "url":"./audio/013.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Endya embi eyinza okuleta ntya endwadde ?",
+         "url":"./audio/014.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Okuggwa'mu omusayi kye'ki ?",
+         "url":"./audio/015.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Biki ebireta anaemia ?",
+         "url":"./audio/016.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Nyinza'ntya okuziyiza okugwamu omusayi ?",
+         "url":"./audio/017.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Beri beri kye'ki ? ",
+         "url":"./audio/018.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Nyinza'ntya okuziyiza beri beri ?",
+         "url":"./audio/019.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Buzibu ki obulala obuva ku kulya emmere enyingi oba okulya ebika by'emmere ebi kyamu ?",
+         "url":"./audio/020.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Obulwadde bwa sukali ky'eki ?",
+         "url":"./audio/021.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Nsobola'ntya okufuga sukali ? ",
+         "url":"./audio/022.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Kiki kyenina okukola okulongosa endya yange ?",
+         "url":"./audio/023.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Oziyiza otya obuzibu mu kukula kwa'abaana, nga kuva ku endya embi ? ",
+         "url":"./audio/024.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Ndisa'ntya abana abato, abasusiza emyezi 6 ?",
+         "url":"./audio/025.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Emeere ki ennungi ku baana abato, abasuka emyezi 6 ?",
+         "url":"./audio/026.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Lwaki ekirisa kya Vitamin A kyamugaso ku mwana wange ?",
+         "url":"./audio/027.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Lwaki iron wa mugaso ku mwana wange ?",
+         "url":"./audio/028.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      {
+         "name":"Lwaki iodine wa mugaso ku mwana wange ?",
+         "url":"./audio/029.mp3",
+         "source": "TPO Uganda",
+         "source_url": "https://tpoug.org/"
+      },
+      
+   ]
+});
