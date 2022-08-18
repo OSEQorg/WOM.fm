75ec34d84a95651c1d05fbda93400b76c4a00008
Marcel Heyne <32398058+marcelheyne@users.noreply.github.com>
2022-08-18 12:22:53 +0200

    Add files via upload

---
+++ 904/README.md
@@ -0,0 +1,13 @@
+# WOM.fm-Digital Financial Literacy In Luo
+
+This is a collection of recorded audio files in Luo purposely educating people on DIGITAL FINANCE .The audios explain the following topics;
+
+- Mobile Money general topics
+- Basic Mobile Money transactions
+- Loans,Savings,Insurance and Budgeting
+- Privacy and Scams
+- Using a Smartphone Effectively
+- Tips for Entrepreneurs 
+
+You can check out the final app by clicking [here](https://womfm-digital-financial-literacy.netlify.app/). 
+
---
+++ 904/audio.js
@@ -0,0 +1,157 @@
+Amplitude.init({
+   "songs":[
+      {
+         "name":"How is the financial system changing?",
+         "url":"./audio/Episode+1+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How do i setup a mobile money account?",
+         "url":"./audio/Episode+2+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+             
+       },
+      {
+         "name":"How do i use my phone for saving?",
+         "url":"./audio/Episode+3+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+   
+      {
+         "name":"How do i send and receive money on my phone?",
+         "url":"./audio/Episode+4+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How do i set up a merchant account for my business?",
+         "url":"./audio/Episode+5+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How do i pay bills on my phone?",
+         "url":"./audio/Episode+6+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How much does it cost to transfer money?",
+         "url":"./audio/Episode+7+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How do i ask for help to temporarily stop repaying my loan?",
+         "url":"./audio/Episode+8+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How do i know which news is true?",
+         "url":"./audio/Episode+9+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How do i protect myself from scams?",
+         "url":"./audio/Episode+10+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How can i reduce cash out fees and avoid agent fraud?",
+         "url":"./audio/Episode+11+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"What are my rights under mobile money terms and conditions?",
+         "url":"./audio/Episode+12+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      }, 
+      {
+         "name":"What should i consder in taking out a loan?",
+         "url":"./audio/Episode+13+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+       {
+         "name":"How should i evaluate digital loan options?",
+         "url":"./audio/Episode+14+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How much should i save?",
+         "url":"./audio/Episode+15+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      }, 
+      {
+         "name":"Where should i save?",
+         "url":"./audio/Episode+16+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"Should i buy insurance?",
+         "url":"./audio/Episode+17+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How can my household manage our finances better?",
+         "url":"./audio/Episode+18+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+       {
+         "name":"How can my personal information be used?",
+         "url":"./audio/Episode+19+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+       {
+         "name":"How can i avoid different types of scams?",
+         "url":"./audio/Episode+20+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How can i figure out airtime and data costs?",
+         "url":"./audio/Episode+21+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How do i use apps?",
+         "url":"./audio/Episode+22+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How should i manage finance for my business?",
+         "url":"./audio/Episode+23+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+       {
+         "name":"How can a digital 'footprint' grow opportunities for my business?",
+         "url":"./audio/Episode+24+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+      {
+         "name":"How can i use my phone to expand sales?",
+         "url":"./audio/Episode+25+Luo.mp3",
+         "source": "Strategic Impact Adivisors",
+         "source_url": "https://www.siaedge.com/lessons"
+      },
+  
+   ]
+});
