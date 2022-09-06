ccaa754d2fcad28e93e4aff32b0919b3808b6a8a
Marcel Heyne <32398058+marcelheyne@users.noreply.github.com>
2022-09-06 15:02:16 +0200

    Update audio.js

--- 902/audio.js
+++ 902/audio.js
@@ -1,37 +1,37 @@
 Amplitude.init({
    "songs":[
       {
-         "name":"TB Awareness - Most people at risk of getting the disease; Everyone is but most especially people who have previously gotten infected, people who are always around the infected ones,kids below 5 years, the elderly 60+ yrs, drunkards, pregnant women and people with diabetes.",
+         "name":"TB Awareness 1",
          "url":"./audio/audio-0.mp3",
          "source": "GLRA Uganda",
          "source_url": ""
       },
       {
-         "name":"TB Awareness - TB is spread through coughing, sneezing, talking at close proximity or by drinking unboiled milk🤔",
+         "name":"TB Awareness 2",
          "url":"./audio/audio-1.mp3",
          "source": "GLRA Uganda",
          "source_url": ""
       },
       {
-         "name":"TB Awareness - How to prevent it. Avoid huge gatherings, cover mouth when coughing,wear masks,take kids to hospital for check up/treatment,boil milk properly.",
+         "name":"TB Awareness 3",
          "url":"./audio/audio-2.mp3",
          "source": "GLRA Uganda",
          "source_url": ""
       },
       {
-         "name":"TB Awareness - TB disease spreads in the air.",
+         "name":"TB Awareness 4",
          "url":"./audio/audio-3.mp3",
          "source": "GLRA Uganda",
          "source_url": ""
       },
       {
-         "name":"TB Awareness - Signs of TB : coughing for more than 2 weeks, sweating at night, weight loss, coughing blood, swelling of body parts like neck, armpits...seek medical attention if you see these signs.",
+         "name":"TB Awareness 5",
          "url":"./audio/audio-4.mp3",
          "source": "GLRA Uganda",
          "source_url": ""
       },
       {
-         "name":"TB Awareness - How to prevent TB:- Avoid huge gatherings, cover mouth when coughing,wear masks,take kids to hospital for check up/treatment,boil milk properly",
+         "name":"TB Awareness 6",
          "url":"./audio/audio-5.mp3",
          "source": "GLRA Uganda",
          "source_url": ""
