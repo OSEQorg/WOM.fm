f1be2fad9f65456b2c8e6f02fef7dde0e7e854fc
Marcel Heyne <32398058+marcelheyne@users.noreply.github.com>
2022-10-28 16:41:49 +0200

    Modifier version of wom.fm/123

---
+++ 124/audio.js
@@ -0,0 +1,40 @@
+Amplitude.init({
+    "songs": [
+      {
+        "name": "Gini mere oji joo njo nke ukwu, iwakpo umu nwanyi?",
+        "url": "./mp3/ig020101.mp3",
+      },
+      
+      {
+        "name": "Gini bu uche na adighi mma gbasara umu nwoke imeru umu nwanyi ahu?",
+        "url": "./mp3/ig020102.mp3",
+      },
+      
+	  {
+        "name": "Gini ga eme nwoke ga eji meruo nwanyi ahu?",
+        "url": "./mp3/ig020103.mp3",
+      },
+      
+      {
+        "name": "Kedu udi mmeru ahu umu nwanyi na erita n’aka umu nwoke dinu?",
+        "url": "./mp3/ig020104.mp3",
+      },
+      
+      {
+        "name": "Kedu ihem kwesiri imata banyere udi ihe m ga ahu, mara na ana emegbu mmadu?",
+        "url": "./mp3/ig020105.mp3",
+      },
+      
+      {
+        "name": "Kedu udi ihe m kwesiri ileputara anya?",
+        "url": "./mp3/ig020106.mp3",
+      },
+      
+      {
+        "name": "Kedu udi nwanyi enwere ike ike megbuo ma wakpo?",
+        "url": "./mp3/ig020107.mp3",
+      }
+
+    ]
+  });
+
