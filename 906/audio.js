25a7a400cd3a29ea4aadc7fe8a5e7271a65e1d5c
Fanny <34095626+Nyayic@users.noreply.github.com>
2022-11-08 21:33:56 +0300

    Updated Audio.js and matched audios
    Updated Audio.js and matched audios

--- 906/audio.js
+++ 906/audio.js
@@ -14,7 +14,7 @@ Amplitude.init({
       },
       {
          "name":"A Silent Crisis-Hausa",
-         "url":"./audio/03-a silent crisis(hausa).omp3",
+         "url":"./audio/03-a silent crisis(hausa).mp3",
          "source": "South Saharan Social Development Organization",
          "source_url": "https://southsaharan.org/"
       },
@@ -27,43 +27,86 @@ Amplitude.init({
      
       {
          "name":"Being Kind to yourself",
-         "url":"./audio/Being_kind_to_yourself.mp3",
+         "url":"./audio/05-Being_kind_to_yourself.mp3",
          "source": "South Saharan Social Development Organization",
          "source_url": "https://southsaharan.org/"
       },
       {
          "name":"Building Awareness",
-         "url":"./audio/Building_Awareness.mp3",
+         "url":"./audio/06-Building_Awareness.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+
+      {
+         "name":"Body Shaming - Igbo",
+         "url":"./audio/07-body_shamming_kncfgd (igbo).mp3",
          "source": "South Saharan Social Development Organization",
          "source_url": "https://southsaharan.org/"
       },
       {
-         "name":"GBV - FAQS?",
-         "url":"./audio/Fifth_question_on_FAQS_n1sqxa.ogg",
+         "name":"Does it make someone Gay?",
+         "url":"./audio/08-does it make someone gay.mp3",
          "source": "South Saharan Social Development Organization",
          "source_url": "https://southsaharan.org/"
       },
       {
-         "name":"GBV - FAQS?",
-         "url":"./audio/First_question_on_FAQS_tbkh4k.ogg",
+         "name":"Doesn't sexual violence only happen to girls.mp3",
+         "url":"./audio/09-doesn't sexual violence only happen to girls.mp3",
          "source": "South Saharan Social Development Organization",
          "source_url": "https://southsaharan.org/"
       },
       {
-         "name":"GBV - FAQS?",
-         "url":"./audio/Third_question_on_FAQS_pgznvk.ogg",
+         "name":"FAQs - GBV?",
+         "url":"./audio/10-First_question_on_FAQS_tbkh4k.mp3",
          "source": "South Saharan Social Development Organization",
          "source_url": "https://southsaharan.org/"
       },
       {
-         "name":"Tips for Youth?",
-         "url":"./audio/tips for youth (hausa).ogg",
+         "name":"FAQs - GBV?",
+         "url":"./audio/11-Second_question_on_FAQS_fgkkjr.mp3",
          "source": "South Saharan Social Development Organization",
          "source_url": "https://southsaharan.org/"
       },
       {
-         "name":"Unhooking Unkind Thoughts",
-         "url":"./audio/Unhooking_from_Unkind_Thoughts.mp3",
+         "name":"FAQs - GBV?",
+         "url":"./audio/12-Third_question_on_FAQS_pgznvk.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"FAQs - GBV?",
+         "url":"./audio/13-Fourth_question_on_FAWS_xn98ks.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"FAQs - GBV?",
+         "url":"./audio/14-Fifth_question_on_FAQS_n1sqxa.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Forms of GBV - Yoruba",
+         "url":"./audio/15-forms of gbv(yoruba).mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Forms of GBV - English",
+         "url":"./audio/16-forms of gender based violence.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Forms of GBV - Igbo",
+         "url":"./audio/17-Forms of Gender Based Violence igbo.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Forms of GBV - Hausa",
+         "url":"./audio/18-forms-of-gender-base-violence-1-(hausa).mp3",
          "source": "South Saharan Social Development Organization",
          "source_url": "https://southsaharan.org/"
       },
