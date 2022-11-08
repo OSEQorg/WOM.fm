fcedba8825b42a82f51f9cf34576be67cc1b09ff
Fanny <34095626+Nyayic@users.noreply.github.com>
2022-11-08 22:15:02 +0300

    Updated Audio.js with all GBV Content
    Updated Audio.js with all GBV Content

--- 906/audio.js
+++ 906/audio.js
@@ -230,5 +230,125 @@ Amplitude.init({
          "source": "South Saharan Social Development Organization",
          "source_url": "https://southsaharan.org/"
       },
+      {
+         "name":"Story of Blessing - Igbo",
+         "url":"./audio/40-story of blessing (igbo).mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Story of Blessing - English",
+         "url":"./audio/41-Story_of_Blessing_a_beneficiary_of_SSDO_zge6e2.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Story of Blessing - Hausa",
+         "url":"./audio/42-story of blessing(hausa).mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Tackling Child abuse and Sexual Harrasment",
+         "url":"./audio/43-Tackling_Child_Abuse_and_Sexual_Harassment_igxg9i.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Tackling Child abuse and Sexual Harrasment-Hausa",
+         "url":"./audio/44-Tackling_child_abuse_and_sexual_harassment(hausa).mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"The Need for Child Protection",
+         "url":"./audio/45-the need for the child protection.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"The Need for Child Protection - Igbo",
+         "url":"./audio/46-The Need for the Child Protection Committee (igbo).mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"The Need for Child Protection - Hausa",
+         "url":"./audio/47-The_need_for_the_child_protection_committe_(hausa).mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Tips for Youth to Prevent GBV - Hausa",
+         "url":"./audio/48-tips for youth (hausa).mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Tips for Youth to Prevent GBV - English",
+         "url":"./audio/49-tips for youth to prevent gbv.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Tips for Youth to Prevent GBV - Igbo",
+         "url":"./audio/50-Tips for Youth to Prevent Gender-Based Violence and Inequality (igbo).mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Tips for Youth to Prevent GBV - Yoruba",
+         "url":"./audio/51-tips(yoruba).mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Unhooking from Unkind Thought",
+         "url":"./audio/52-Unhooking_from_Unkind_Thoughts.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"UNICEF",
+         "url":"./audio/53-unicef-1_68GJJvcZ.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"What Happens Afterwards?",
+         "url":"./audio/54-what happens afterwards.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"What is GBV? - Igbo",
+         "url":"./audio/55-what is gbv (igbo).mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"What is GBV? - Yoruba",
+         "url":"./audio/56-What_is_GBV_yoruba.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"What is GBV? - English",
+         "url":"./audio/57-what is gender based violence.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"What is GBV? - Hausa",
+         "url":"./audio/58-What_is_gender_base_violence_(Hausa).mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
+      {
+         "name":"Whose fault is it?",
+         "url":"./audio/59-whose fault is it.mp3",
+         "source": "South Saharan Social Development Organization",
+         "source_url": "https://southsaharan.org/"
+      },
    ]
 });
