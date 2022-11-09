9f21ac759bbd065aa0bb088858a57b7730db2777
Fanny <34095626+Nyayic@users.noreply.github.com>
2022-11-09 11:01:22 +0300

    Added 909-Sex education
    Added 909-Sex education, renamed audios

---
+++ 909/audio.js
@@ -0,0 +1,61 @@
+Amplitude.init({
+   "songs":[
+      {
+         "name":"Sex Education and Proper Menstrual Hygiene - Awareness in English Language",
+         "url":"./audio/01-awareness_in_english.mp3",
+         "source": "Debachanya Girl Empowerment Initiative",
+         "source_url": "https://drive.google.com/file/d/1vNg4JKrrahQZ9yEWrRqNEZRiM3BHzAIu/view?usp=drivesdk"
+      },
+      {
+         "name":"Sex Education and Proper Menstrual Hygiene - Awareness in Yoruba Language",
+         "url":"./audio/02-awareness_in_yoruba.mp3",
+         "source": "Debachanya Girl Empowerment Initiative",
+         "source_url": "https://drive.google.com/file/d/1vNV8K6cuuzKO3VVxm3LymCgkdnPG9JMb/view?usp=drivesdk"
+      },
+      {
+         "name":"Sex Education and Proper Menstrual Hygiene - Awareness in Hausa Language",
+         "url":"./audio/09-sex_education.mp3",
+         "source": "Debachanya Girl Empowerment Initiative",
+          "source_url": "https://drive.google.com/file/d/1-mD49NgTkODDAbmkL_VZHbbMraMvwMSz/view?usp=drivesdk"
+     
+      },
+      {
+         "name":"Child Marriage - Awareness in English Language",
+         "url":"./audio/04-child_marriageEnglish.mp3",
+         "source": "Debachanya Girl Empowerment Initiative",
+         "source_url": "https://drive.google.com/file/d/1ulVz_hYn7L8iWKRv1QrrG_AGuyVPgzlu/view?usp=drivesdk"
+      },
+      {
+        "name":"Child Marriage - Awareness in Yoruba Language",
+         "url":"./audio/03-child_marriage_yoruba.mp3",
+         "source": "Debachanya Girl Empowerment Initiative",
+         "source_url": "https://drive.google.com/file/d/11DwCv7hpYkr_HsKeYYcZ4CWoY1rBlEHi/view?usp=drivesdk"
+      },
+      {
+         "name":"Child Marriage - Awareness in Hausa Language",
+         "url":"./audio/05-childMarriageHausa.mp3",
+         "source": "Debachanya Girl Empowerment Initiative",
+         "source_url": "https://drive.google.com/file/d/1rB9BpODvuAEuVlX5YWa7r2LZUMiIDZx4/view?usp=drivesdk"
+      },
+      {
+         "name":"Digital Literacy - Awareness in English Language",
+         "url":"./audio/06-Digital_literacy_English.mp3",
+         "source": "Debachanya Girl Empowerment Initiative",
+         "source_url": "https://drive.google.com/file/d/16iNby6kMFaPzZd6rG8-u8DsZAg3Rw5Ra/view?usp=drivesdk"
+      },
+      {
+         "name":"Digital Literacy - Awareness in Yoruba Language",
+         "url":"./audio/07-digital_literacy_yoruba.mp3",
+         "source": "Debachanya Girl Empowerment Initiative",
+         "source_url": "https://drive.google.com/file/d/1LmLYFrGuEEPWXnL0xSf52dQcTie-whkc/view?usp=drivesdk"
+      },
+      {
+         "name":"Digital Literacy - Awareness in Hausa Language",
+         "url":"./audio/08-Digital_literacy.mp3",
+         "source": "Debachanya Girl Empowerment Initiative",
+         "source_url": "https://drive.google.com/file/d/1WZRUrv0xVCc5gSNP5UVZNHpkHQj9cUUy/view?usp=drivesdk"
+      }
+     
+
+   ]
+});
