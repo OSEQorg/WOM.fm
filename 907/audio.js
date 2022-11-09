792a3639f0c58c1e941a7ed5b87efcfdd3756619
Fanny <34095626+Nyayic@users.noreply.github.com>
2022-11-09 09:44:33 +0300

    Added Content on Nutrition-907
    Added Content on Nutrition - team extra ordinary

---
+++ 907/audio.js
@@ -0,0 +1,48 @@
+Amplitude.init({
+   "songs":[
+      {
+         "name":"Introduction",
+         "url":"./audio/01-intro-mulnutrition reduction.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://lagosfoodbank.org/"
+      },
+      {
+         "name":"Malnutrition Reduction - LFBI Testimonial 1",
+         "url":"./audio/02-LFBI-1-woman_edited.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://lagosfoodbank.org/"
+      },
+      
+      {
+         "name":"Malnutrition Reduction - LFBI Testimonial 2",
+         "url":"./audio/03-LFBI-2-women_edited.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://lagosfoodbank.org/"
+      },
+      {
+         "name":"Malnutrition Reduction - LFBI Testimonial 3",
+         "url":"./audio/04-LFBI-4-Funmi-edited.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://lagosfoodbank.org/"
+      },
+      {
+         "name":"Malnutrition Reduction - Tips to curb nutrition",
+         "url":"./audio/05-funmi-merged.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://lagosfoodbank.org/"
+      },
+      {
+         "name":"Malnutrition Reduction - Tips to curb nutrition",
+         "url":"./audio/06-emily-tee.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://lagosfoodbank.org/"
+      },
+      {
+         "name":"Malnutrition Reduction - Tips to curb nutrition",
+         "url":"./audio/07-emily-final.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://lagosfoodbank.org/"
+      }
+  
+   ]
+});
