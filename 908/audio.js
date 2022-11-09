954e8370e51b16fac36f6b937ad9619b88717fa0
Fanny <34095626+Nyayic@users.noreply.github.com>
2022-11-09 10:26:09 +0300

    Converted audios from .m4a to mp3
    Converted all audios for positive parenting from .m4a to mp3

--- 908/audio.js
+++ 908/audio.js
@@ -2,50 +2,50 @@ Amplitude.init({
    "songs":[
       {
          "name":"Good Habits for Kids",
-         "url":"./audio/01-good_habits_for_kids.m4a",
+         "url":"./audio/01-good_habits_for_kids.mp3",
          "source": "Positive Parenting",
          "source_url": "https://www.positiveparentingsolutions.com/"
       },
       {
          "name":"How to Talk to your Kids - English",
-         "url":"./audio/02-how_to_talk_to_your_kids.aac",
+         "url":"./audio/02-how_to_talk_to_your_kids.mp3",
          "source": "Positive Parenting",
          "source_url": "https://www.positiveparentingsolutions.com/"
       },
       
       {
          "name":"How to Talk to your Kids - Yoruba",
-         "url":"./audio/03-how_to_talk_to_your_kids_yoruba.aac",
+         "url":"./audio/03-how_to_talk_to_your_kids_yoruba.mp3",
          "source": "Positive Parenting",
          "source_url": "https://www.positiveparentingsolutions.com/"
       },
       {
          "name":"Hygiene habits for Kids - English",
-         "url":"./audio/04-hygiene_habits_for_kids.aac",
+         "url":"./audio/04-hygiene_habits_for_kids.mp3",
          "source": "Positive Parenting",
          "source_url": "https://www.positiveparentingsolutions.com/"
       },
       {
          "name":"Teaching Good Manners to Children - English",
-         "url":"./audio/05-teaching_good_manners_to_children.aac",
+         "url":"./audio/05-teaching_good_manners_to_children.mp3",
          "source": "Positive Parenting",
          "source_url": "https://www.positiveparentingsolutions.com/"
       },
       {
          "name":"How to Talk to your Kids - Igbo",
-         "url":"./audio/06-how_to_talk_to_your_kids_igbo.mp4",
+         "url":"./audio/06-how_to_talk_to_your_kids_igbo.mp3",
          "source": "Positive Parenting",
          "source_url": "https://www.positiveparentingsolutions.com/"
       },
       {
          "name":"Teaching Good manners to Children - Igbo",
-         "url":"./audio/07-teaching_good_manners_to_children_igbo.m4a",
+         "url":"./audio/07-teaching_good_manners_to_children_igbo.mp3",
          "source": "Positive Parenting",
          "source_url": "https://www.positiveparentingsolutions.com/"
       },
       {
          "name":"Teaching Good manners to Children - Yoruba",
-         "url":"./audio/07-teaching_good_manners_to_children_igbo.m4a",
+         "url":"./audio/07-teaching_good_manners_to_children_igbo.mp3",
          "source": "Positive Parenting",
          "source_url": "https://www.positiveparentingsolutions.com/"
       }
